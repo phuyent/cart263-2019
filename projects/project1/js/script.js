af6857b4dac8da16e471339704a6d2735517b9f4
phuyent <huyentran317@gmail.com>
2019-02-20 05:42:02 -0500

    Project1-PART2
    Adding background, visuals,  setting up the basics

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -1,11 +1,54 @@
-"use strict";
-
 /*****************
 
-Title of Project
-Author Name
+Project 1: Happiness is Sysiphus?
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Huyen Tran Pham (Emma)
 
+My idea in this game is to make the player to be Zeus,
+the one who decide Sysiphus'fate.
+Therefore, the player will play to resize the rock of Sysiphus,
+it will either be a tiny stone or an enormous rock :)
+
+
+Let's begin
 ******************/
+
+"use strict";
+
+//Claim the varibles holding images from html page
+let $stone;
+let $plusStone;
+let $minusStone;
+let $bg;
+
+
+
+//The program will run when the function is ready
+$(document).ready(setup);
+
+
+//setup()
+//
+//Using jQuery UI to drag the stones
+function setup() {
+
+  $plusStone = $('#plusStone');
+  $plusStone.draggable();
+
+  $minusStone = $('#minusStone');
+  $minusStone.draggable();
+
+  $stone = $('#stone');
+  $stone.droppable({
+    drop: stoneDropped
+  });
+
+}
+
+//drop()
+//
+//Using jQuery UI to drop the stones to the rock
+function stoneDropped (event,ui) {
+  ui.draggable.remove();
+  $(this).attr('src','assets/images/grey.png');
+}
