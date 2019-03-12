df58e680cbfd7219c96831bbb19387124a135c78
phuyent <43248395+phuyent@users.noreply.github.com>
2019-03-12 12:13:20 -0400

    Ex6-Part4
    Fixed the issue of using only 'a' as the infinite article

--- assignments/assignment6/js/script.js
+++ assignments/assignment6/js/script.js
@@ -16,7 +16,6 @@ http://rednoise.org/rita/index.html
 
 */
 
-let vowels = "aeiou";
 
 $(document).ready(function() {
 
@@ -48,7 +47,6 @@ function gotData(data) {
     // If so, assume it's plural (this is a flawed assumption)
     verb = 'are';
   }
-
   // Now the cat
   let cat = getRandomElement(data.cats);
 
@@ -62,10 +60,23 @@ function gotData(data) {
   let fruit = getRandomElement(data.fruits);
 
 
+//Set 'a' to be the default article
+ let article = 'a';
+//Check if the first letter of the fruit is a vowel
+  if (fruit.charAt(0) === 'a' || fruit.charAt(0) === 'e' || fruit.charAt(0) === 'i' ||
+fruit.charAt(0) === 'o' || fruit.charAt(0) === 'u') {
+    article = 'an';
+  }
+  //Check if the first letter of the pastry is a vowel
+  if (pastry.charAt(0) === 'a' || pastry.charAt(0) === 'e' || pastry.charAt(0) === 'i' ||
+pastry.charAt(0) === 'o' || pastry.charAt(0) === 'u') {
+      article = 'an';
+    }
+
   // Now we can construct our description with a template string
   // We have the basic structure of a sentence and we substitute in the
   // values we've just calculated
-  let description = `${condiment} ${verb} like a ${fruit} ${cat} in a ${pastrie} ${room}.`;
+  let description = `${condiment} ${verb} like ${article} ${fruit} ${cat} in ${article} ${pastry} ${room}.`;
 
   // Finally, we add it to the page and hey presto!
   $('body').append(description)
